f1aee413f330863c8d79b64c8ea20b8928008810
noorahmedfarid <noor.ahmed.farid@gmail.com>
2020-03-12 15:24:45 +1100

    my first commit

---
+++ src/build/precache-manifest.1627d0310c6cf5153e04a069cd9fca90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e1c44acfc60c884adec",
+    "url": "/static/js/main.b94f932e.chunk.js"
+  },
+  {
+    "revision": "09e664e88f5737ba9211",
+    "url": "/static/js/2.20e07d70.chunk.js"
+  },
+  {
+    "revision": "1e1c44acfc60c884adec",
+    "url": "/static/css/main.07e34592.chunk.css"
+  },
+  {
+    "revision": "9599f89b6a8209a75a413937ffc39396",
+    "url": "/index.html"
+  }
+];
